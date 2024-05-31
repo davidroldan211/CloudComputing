c2b815b77cd87600306cd423d6dfc2c03f88031e
David Roldán Vélez <54605554+davidroldan211@users.noreply.github.com>
2024-05-31 11:33:09 -0500

    Update database.py
    Eliminar varibles de la base de datos

--- database.py
+++ database.py
@@ -2,10 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = "admin"
-DB_PASSWORD = "admin1234"
-DB_HOST = "database-2.crp74docgkpu.us-east-1.rds.amazonaws.com"
-DB_NAME = "database-2"
+
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
